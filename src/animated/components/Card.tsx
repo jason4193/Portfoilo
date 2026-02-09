67bc245446863136fedc5a6d5d88d545d5d32f40
jason4193 <z5391104@ad.unsw.edu.au>
2026-02-09 19:11:36 +1100

    feat: Add new Portfolio model and update Card component to reference the latest model version

--- src/animated/components/Card.tsx
+++ src/animated/components/Card.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useRef } from "react";
 import gsap from "gsap";
 import { useMediaQuery } from "react-responsive";
-import PortfolioCardModel from "../models/PortfolioCardModel_v2";
+import PortfolioCardModel from "../models/PortfolioCardModel_v3";
 import type { Group } from "three";
 import {
   CARD_SCALE_DESKTOP,
